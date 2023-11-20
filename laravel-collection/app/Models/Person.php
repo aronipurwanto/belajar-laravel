14729083265d5c297c9ceb8599fb364b43b306c5
roni.purwanto@amartha.com <roni.purwanto@amartha.com>
2023-11-20 21:31:37 +0700

    feat: complete eloquest database

--- laravel-collection/app/Models/Person.php
+++ laravel-collection/app/Models/Person.php
@@ -4,5 +4,15 @@ namespace App\Models;
 
 class Person
 {
+    public string $name;
+    public string $department;
+
+    /**
+     * @param string $name
+     */
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+    }
 
 }
